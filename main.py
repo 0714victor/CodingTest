f0a03bc1722284e4d60e6d4e36b28ea269a537be
Victor <0714victor@gmail.com>
2023-01-18 23:22:17 -0500

    create transform function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import warnings
+import re
 import time
 
 MONTH_CODES = "FGHJKMNQUVXZ"
@@ -60,6 +62,66 @@ def transform(raw_data_: pd.DataFrame, instruments_: pd.DataFrame) -> pd.DataFra
     :param raw_data_: dataframe of all features associated with instruments, with associated timestamps
     :param instruments_: dataframe of all traded instruments
     """
+    df = raw_data_.copy()
+    if 'Error' in df.columns:
+        df = df[df['error'] != 'Not Found']
+    df['contract'] = np.where(df['Term'] == None, df['Period'], df['Term'])
+    df['Trade Date'] = pd.to_datetime(df['Trade Date'])
+    df['Expiration Date ts'] = pd.to_datetime(df['Expiration Date'])
+    if df['Trade Date'].isnull().values.any():
+        warnings.warn('some nulls in Trade Date')
+        df = df.dropna(subset = ['Trade Date'])
+    if not df[df['Trade Date'] > df['Expiration Date ts']].empty:
+        warnings.warn('some trades in expired instruments')
+        df = df[df['Trade Date'] <= df['Expiration Date ts']]
+    if df['contract'].isnull().values.any():
+        warnings.warn('some nulls in contract')
+        df = df.dropna(subset = ['contract'])
+
+    base_list = instruments_['Base']
+    def get_base(ric: str) -> str:
+        for b in base_list:
+            if ric.startswith(b) & ric[len(b)].isnumeric():
+                return b
+    df['base'] = df['RIC'].map(get_base)
+
+    def get_moneyness(ric: str) -> int:
+        for b in base_list:
+            if ric.startswith(b):
+                s = ric[len(b):]
+                res = re.findall('\d+', s)
+                return res[0]
+    df['moneyness'] = df['RIC'].map(get_moneyness)
+    df = df.merge(instruments_, left_on = 'base', right_on = 'Base')
+    df['contract_year'] = df['Expiration Date'].map(lambda x: x[6:10])
+    df['contract_month'] = df['Period'].map(lambda x: x[0:3])
+
+    def adjust_contract_year(row):
+        if row['contract_year'][3] != row['Period'][3]:
+            return int(row['contract_year']) + 1
+        return row['contract_year']
+    df['contract_year'] = df.apply(lambda row: adjust_contract_year(row), axis=1)
+
+    df['month_code'] = df['contract_month'].map(MONTH_NAME_TO_CODE.get)
+    df = df.rename(columns = FIELDS_MAP)
+    df['Bloomberg Ticker'] = df['Bloomberg Ticker'].map(lambda x: x + '_' if len(x) == 1 else x)
+    df['symbol'] = ('FUTURE_VOL_' + df['Exchange'] + '_' + df['Bloomberg Ticker'] +
+                    df['month_code'] + df['contract_year'].astype(str) + '_' + df['moneyness'].astype(str))
+    df['source'] = 'refinitiv'
+    names = ['date', 'symbol', 'source'] + FLOAT_FIELDS
+    df = df[names]
+    df = df.set_index(['date', 'symbol', 'source'])
+    stack_name = (['value'] * len(FLOAT_FIELDS))
+    idx = pd.MultiIndex.from_arrays([stack_name, FLOAT_FIELDS], names = ['value', 'field'])
+    df.columns = idx
+    df = df.stack(level = ['field'])
+    df.columns.names = [None]
+
+    df = df.sort_values(by = 'field', kind = 'stable', key = lambda x: x.map(FLOAT_FIELDS.index))
+    df = df.reset_index()
+    df['value'] = df['value'].astype(str).map(lambda x: x.replace(',','')).astype(float)
+
+    return df
     pass
 
 
